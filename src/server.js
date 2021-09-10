c45391dbc58d9f2db5080a7d2840c4db1113ec9d
jinwook-song <wlsdnr129@naver.com>
2021-09-10 20:13:24 +0900

    lecture 3. Middlewares

--- src/server.js
+++ src/server.js
@@ -4,6 +4,22 @@ const PORT = 3000;
 
 const app = express();
 
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  next();
+};
+
+// Global middleware
+app.use(loggerMiddleware);
+app.use(privateMiddleware);
+
 app.get("/", (req, res) => res.send("Welcome"));
 app.get("/login", (req, res) => res.send("Login Here"));
 
