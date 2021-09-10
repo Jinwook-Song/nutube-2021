01c8144da2d56b3527a1f2ef49768462eedbb15e
jinwook-song <wlsdnr129@naver.com>
2021-09-10 20:53:18 +0900

    lecture 3. Morgan

--- src/server.js
+++ src/server.js
@@ -1,35 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 3000;
 
 const app = express();
-
-const loggerMiddleware = (req, res, next) => {
-  console.log(`${req.method}: ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  next();
-};
-const redirectMiddleware = (req, res, next) => {
-  console.log("You can't enter there");
-  return res.redirect("/");
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res) => res.send("<h1>Welcome</h1>");
 const handleLogin = (req, res) => res.send("Login Here");
 
-// Global middleware
-app.use(loggerMiddleware, privateMiddleware);
+app.use(logger);
 
 app.get("/", handleHome);
 app.get("/login", handleLogin);
-
-app.use(redirectMiddleware);
 
 const handleListening = () =>
   console.log(`Listening on http://localhost:${PORT} ✅`);
