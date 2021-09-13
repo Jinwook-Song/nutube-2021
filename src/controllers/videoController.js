99b63c1a6d8c2dbd164bd6086e0dd1c8bef3a4f4
jinwook-song <wlsdnr129@naver.com>
2021-09-13 20:56:48 +0900

    lecture 6. Shema Validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,15 +31,13 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      createdAt: Date.now(),
       hashtags: hashtags.split(",").map((word) => `#${word}`),
-      meta: {
-        views: 0,
-        rating: 0,
-      },
     });
   } catch (error) {
-    console.log(error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
   }
 
   return res.redirect("/");
