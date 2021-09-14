3cc6498545fe69bcf6d75d2abfa90be3f3e282fc
jinwook-song <wlsdnr129@naver.com>
2021-09-14 20:54:00 +0900

    lecture 7. Check account validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,21 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const existsAccount = await User.exists({ $or: [{ username }, { email }] });
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  if (existsAccount) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
   await User.create({
     name,
     username,
