e2d38256b369541bce5718a816f87e58e3c8acce
jinwook-song <wlsdnr129@naver.com>
2021-09-15 19:58:31 +0900

    lecture 7. Sessions and Cookies

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -11,6 +12,26 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views/pages");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.count += 1;
+  return res.send(`${req.session.id} ${req.session.count}`);
+});
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
