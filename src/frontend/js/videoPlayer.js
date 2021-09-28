e6caada88d9d0059c7cd58d1ebb72d372ac8f0fa
jinwook-song <wlsdnr129@naver.com>
2021-09-28 18:15:55 +0900

    lecture 10. Play, Pause

--- src/frontend/js/videoPlayer.js
+++ src/frontend/js/videoPlayer.js
@@ -1,1 +1,18 @@
-console.log("video player");
+const video = document.querySelector("video"),
+  playBtn = document.querySelector("#play"),
+  muteBtn = document.querySelector("#mute"),
+  time = document.querySelector("#time"),
+  volume = document.querySelector("#volume");
+
+const handlePlayClick = (event) =>
+  video.paused ? video.play() : video.pause();
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (evnet) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
