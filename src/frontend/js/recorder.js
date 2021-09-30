8803a1f4220597d0220c23b7cfc625cc7eaa4a62
jinwook-song <wlsdnr129@naver.com>
2021-09-30 20:31:00 +0900

    lecture 13. Recording Video

--- src/frontend/js/recorder.js
+++ src/frontend/js/recorder.js
@@ -1,13 +1,45 @@
 const startBtn = document.querySelector("#startBtn");
 const video = document.querySelector("#preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+
+const handleDownload = () => {};
+
+const hanldeStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", hanldeStop);
+  startBtn.addEventListener("click", handleDownload);
+
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", hanldeStop);
+
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log(event.data);
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
