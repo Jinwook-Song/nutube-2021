b2dfe7077fa1a8ad6bb0b1e128057a16ec2a4bd8
jinwook-song <wlsdnr129@naver.com>
2021-09-30 21:57:44 +0900

    lecture 14. Thumbnail

--- src/frontend/js/recorder.js
+++ src/frontend/js/recorder.js
@@ -8,7 +8,11 @@ let recorder;
 let videoFile;
 
 const handleDownload = async () => {
-  // virtual environment
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleDownload);
+  startBtn.addEventListener("click", handleStart);
+
+  ////////////////////// virtual environment //////////////////////
   const ffmpeg = createFFmpeg({
     corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
     log: true,
@@ -20,23 +24,43 @@ const handleDownload = async () => {
   // convert and encode with 60 frame
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
 
+  // screenshot
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg"
+  );
+
   const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
 
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
 
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  /////////////////////////////////////////////////////////////////
-  startBtn.innerText = "Start Recording";
-  startBtn.removeEventListener("click", handleDownload);
-  startBtn.addEventListener("click", handleStart);
+  ////////////////////// virtual environment //////////////////////
 
-  const a = document.createElement("a");
-  a.href = mp4Url;
-  a.download = "My Recording.mp4";
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+  // for Video
+  const videoA = document.createElement("a");
+  videoA.href = mp4Url;
+  videoA.download = "My Recording.mp4";
+  document.body.appendChild(videoA);
+  videoA.click();
+  document.body.removeChild(videoA);
+
+  // for Thumbnail
+  const thumbnailA = document.createElement("a");
+  thumbnailA.href = thumbUrl;
+  thumbnailA.download = "My Thumbnail.jpg";
+  document.body.appendChild(thumbnailA);
+  thumbnailA.click();
+  document.body.removeChild(thumbnailA);
 
   init();
 };
