5423f8c304c71eceabf885df182c86358197cf63
jinwook song <wlsdnr129@naver.com>
2024-06-06 17:03:12 +0900

    update dependencies

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,23 +1,21 @@
-import User from "../models/User";
-import Video from "../models/Video";
-import fetch from "node-fetch";
-import bcrypt from "bcrypt";
+import User from '../models/User';
+import bcrypt from 'bcrypt';
 
 // Join
-export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   const existsAccount = await User.exists({ $or: [{ username }, { email }] });
   if (password !== password2) {
-    return res.status(400).render("join", {
-      pageTitle: "Join",
-      errorMessage: "Password confirmation does not match.",
+    return res.status(400).render('join', {
+      pageTitle: 'Join',
+      errorMessage: 'Password confirmation does not match.',
     });
   }
   if (existsAccount) {
-    return res.status(400).render("join", {
-      pageTitle: "Join",
-      errorMessage: "This username/email is already taken.",
+    return res.status(400).render('join', {
+      pageTitle: 'Join',
+      errorMessage: 'This username/email is already taken.',
     });
   }
   try {
@@ -28,10 +26,10 @@ export const postJoin = async (req, res) => {
       password,
       location,
     });
-    return res.redirect("/login");
+    return res.redirect('/login');
   } catch (error) {
-    return res.status(400).render("join", {
-      pageTitle: "Join",
+    return res.status(400).render('join', {
+      pageTitle: 'Join',
       errorMessage: error._message,
     });
   }
@@ -39,44 +37,44 @@ export const postJoin = async (req, res) => {
 
 // Login
 export const getLogin = (req, res) =>
-  res.render("login", { pageTitle: "Login" });
+  res.render('login', { pageTitle: 'Login' });
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username, githubLogin: false });
   if (!user) {
-    return res.status(400).render("login", {
-      pageTitle: "Login",
-      errorMessage: "An account with this username does not exists.",
+    return res.status(400).render('login', {
+      pageTitle: 'Login',
+      errorMessage: 'An account with this username does not exists.',
     });
   }
   const ok = await bcrypt.compare(password, user.password);
   if (!ok) {
-    return res.status(400).render("login", {
-      pageTitle: "Login",
-      errorMessage: "Wrong password.",
+    return res.status(400).render('login', {
+      pageTitle: 'Login',
+      errorMessage: 'Wrong password.',
     });
   }
   // give some informations to Each browser's server session object
   req.session.loggedIn = true;
   req.session.user = user;
-  req.flash("info", `Hello ${user.username}.`);
-  return res.redirect("/");
+  req.flash('info', `Hello ${user.username}.`);
+  return res.redirect('/');
 };
 
 // Github Login (https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps)
 export const startGithubLogin = (req, res) => {
-  const baseUrl = "https://github.com/login/oauth/authorize";
+  const baseUrl = 'https://github.com/login/oauth/authorize';
   const config = {
     client_id: process.env.GH_CLIENT,
     allow_signup: false,
-    scope: "read:user user:email",
+    scope: 'read:user user:email',
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
   return res.redirect(finalUrl);
 };
 export const finishGithubLogin = async (req, res) => {
-  const baseUrl = "https://github.com/login/oauth/access_token";
+  const baseUrl = 'https://github.com/login/oauth/access_token';
   const config = {
     client_id: process.env.GH_CLIENT,
     client_secret: process.env.GH_SECRET,
@@ -86,15 +84,15 @@ export const finishGithubLogin = async (req, res) => {
   const finalUrl = `${baseUrl}?${params}`;
   const tokenRequest = await (
     await fetch(finalUrl, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Accept: "application/json",
+        Accept: 'application/json',
       },
     })
   ).json();
-  if ("access_token" in tokenRequest) {
+  if ('access_token' in tokenRequest) {
     const { access_token } = tokenRequest;
-    const apiUrl = "https://api.github.com";
+    const apiUrl = 'https://api.github.com';
     const userData = await (
       await fetch(`${apiUrl}/user`, {
         headers: {
@@ -110,12 +108,12 @@ export const finishGithubLogin = async (req, res) => {
       })
     ).json();
     const emailObj = emailData.find(
-      (email) => email.primary === true && email.verified === true
+      (email) => email.primary === true && email.verified === true,
     );
     if (!emailObj) {
       // set notification
-      req.flash("error", "Email doesn't exist.");
-      return res.redirect("/login");
+      req.flash('error', "Email doesn't exist.");
+      return res.redirect('/login');
     }
     let user = await User.findOne({ email: emailObj.email });
     if (!user) {
@@ -124,7 +122,7 @@ export const finishGithubLogin = async (req, res) => {
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
-        password: "",
+        password: '',
         githubLogin: true,
         avatarUrl: userData.avatar_url,
         location: userData.location,
@@ -133,25 +131,25 @@ export const finishGithubLogin = async (req, res) => {
     // logged user in
     req.session.loggedIn = true;
     req.session.user = user;
-    req.flash("info", `Hello ${user.username}.`);
-    return res.redirect("/");
+    req.flash('info', `Hello ${user.username}.`);
+    return res.redirect('/');
   } else {
-    return res.redirect("/login");
+    return res.redirect('/login');
   }
 };
 
 // Logout
 export const logout = (req, res) => {
-  req.flash("info", "Bye Bye ~");
+  req.flash('info', 'Bye Bye ~');
   req.session.loggedIn = false;
   req.session.user = null;
   // req.session.destroy();
-  return res.redirect("/");
+  return res.redirect('/');
 };
 
 // Edit Profile
 export const getEdit = (req, res) => {
-  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+  return res.render('edit-profile', { pageTitle: 'Edit Profile' });
 };
 export const postEdit = async (req, res) => {
   const {
@@ -170,20 +168,20 @@ export const postEdit = async (req, res) => {
       username,
       location,
     },
-    { new: true }
+    { new: true },
   );
   req.session.user = updatedUser;
-  req.flash("success", "Updated Profile.");
+  req.flash('success', 'Updated Profile.');
   return res.redirect(`/users/${id}`);
 };
 
 // Change Password
 export const getChangePassword = (req, res) => {
   if (req.session.user.githubLogin === true) {
-    req.flash("error", "Can't change password.");
-    return res.redirect("/");
+    req.flash('error', "Can't change password.");
+    return res.redirect('/');
   }
-  return res.render("change-password", { pageTitle: "Change Password" });
+  return res.render('change-password', { pageTitle: 'Change Password' });
 };
 export const postChangePassword = async (req, res) => {
   const {
@@ -195,21 +193,21 @@ export const postChangePassword = async (req, res) => {
   const user = await User.findById(id);
   const ok = await bcrypt.compare(oldPW, user.password);
   if (!ok) {
-    return res.status(400).render("change-password", {
-      pageTitle: "Change Password",
-      errorMessage: "The current password is incorrect.",
+    return res.status(400).render('change-password', {
+      pageTitle: 'Change Password',
+      errorMessage: 'The current password is incorrect.',
     });
   }
   if (newPW !== newPW2) {
-    return res.status(400).render("change-password", {
-      pageTitle: "Change Password",
-      errorMessage: "The password does not match the confirmation.",
+    return res.status(400).render('change-password', {
+      pageTitle: 'Change Password',
+      errorMessage: 'The password does not match the confirmation.',
     });
   }
   user.password = newPW;
   await user.save(); // triger the pre middleware
   // send notification
-  req.flash("success", "Password updated.");
+  req.flash('success', 'Password updated.');
   return res.redirect(`/users/${id}`);
 };
 
@@ -217,16 +215,16 @@ export const postChangePassword = async (req, res) => {
 export const see = async (req, res) => {
   const { id } = req.params;
   const user = await User.findById(id).populate({
-    path: "videos",
+    path: 'videos',
     populate: {
-      path: "owner",
-      model: "User",
+      path: 'owner',
+      model: 'User',
     },
   });
   if (!user) {
-    return res.status(404).render("404", { pageTitle: "User not found." });
+    return res.status(404).render('404', { pageTitle: 'User not found.' });
   }
-  return res.render("profile", {
+  return res.render('profile', {
     pageTitle: `${user.username}'s Profile`,
     user,
   });
--- src/frontend/js/recorder.js
+++ src/frontend/js/recorder.js
@@ -1,20 +1,21 @@
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { fetchFile } from '@ffmpeg/util';
 
-const actionBtn = document.querySelector("#actionBtn");
-const video = document.querySelector("#preview");
+const actionBtn = document.querySelector('#actionBtn');
+const video = document.querySelector('#preview');
 
 let stream;
 let recorder;
 let videoFile;
 
 const files = {
-  input: "recording.webm",
-  output: "output.mp4",
-  thumb: "thumbnail.jpg",
+  input: 'recording.webm',
+  output: 'output.mp4',
+  thumb: 'thumbnail.jpg',
 };
 
 const downloadFile = (fileUrl, fileName) => {
-  const a = document.createElement("a");
+  const a = document.createElement('a');
   a.href = fileUrl;
   a.download = fileName;
   document.body.appendChild(a);
@@ -23,38 +24,38 @@ const downloadFile = (fileUrl, fileName) => {
 };
 
 const handleDownload = async () => {
-  actionBtn.innerText = "Transcoding...";
-  actionBtn.removeEventListener("click", handleDownload);
+  actionBtn.innerText = 'Transcoding...';
+  actionBtn.removeEventListener('click', handleDownload);
   actionBtn.disabled = true;
 
   ////////////////////// virtual environment //////////////////////
-  const ffmpeg = createFFmpeg({
-    corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
+  const ffmpeg = new FFmpeg({
+    corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js',
     log: true,
   });
   await ffmpeg.load();
 
-  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+  await ffmpeg.writeFile(files.input, await fetchFile(videoFile));
 
   // convert and encode with 60 frame
-  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
+  await ffmpeg.exec(['-i', files.input, '-r', '60', files.output]);
 
   // screenshot
-  await ffmpeg.run(
-    "-i",
+  await ffmpeg.exec([
+    '-i',
     files.input,
-    "-ss",
-    "00:00:01",
-    "-frames:v",
-    "1",
-    files.thumb
-  );
+    '-ss',
+    '00:00:01',
+    '-frames:v',
+    '1',
+    files.thumb,
+  ]);
 
-  const mp4File = ffmpeg.FS("readFile", files.output);
-  const thumbFile = ffmpeg.FS("readFile", files.thumb);
+  const mp4File = await ffmpeg.readFile(files.output);
+  const thumbFile = await ffmpeg.readFile(files.thumb);
 
-  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
-  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+  const mp4Blob = new Blob([mp4File.buffer], { type: 'video/mp4' });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: 'image/jpg' });
 
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
@@ -62,35 +63,35 @@ const handleDownload = async () => {
   ////////////////////// virtual environment //////////////////////
 
   // Download video and thumnail
-  downloadFile(mp4Url, "My Recording.mp4");
-  downloadFile(thumbUrl, "My Thumbnail.jpg");
+  downloadFile(mp4Url, 'My Recording.mp4');
+  downloadFile(thumbUrl, 'My Thumbnail.jpg');
 
   // Clear memory
-  ffmpeg.FS("unlink", files.input);
-  ffmpeg.FS("unlink", files.output);
-  ffmpeg.FS("unlink", files.thumb);
+  await ffmpeg.deleteFile(files.input);
+  await ffmpeg.deleteFile(files.output);
+  await ffmpeg.deleteFile(files.thumb);
 
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
   URL.revokeObjectURL(videoFile);
 
   actionBtn.disabled = false;
-  actionBtn.innerText = "Record Again";
-  actionBtn.addEventListener("click", handleStart);
+  actionBtn.innerText = 'Record Again';
+  actionBtn.addEventListener('click', handleStart);
 };
 
 const hanldeStop = () => {
-  actionBtn.innerText = "Download Recording";
-  actionBtn.removeEventListener("click", hanldeStop);
-  actionBtn.addEventListener("click", handleDownload);
+  actionBtn.innerText = 'Download Recording';
+  actionBtn.removeEventListener('click', hanldeStop);
+  actionBtn.addEventListener('click', handleDownload);
 
   recorder.stop();
 };
 
 const handleStart = () => {
-  actionBtn.innerText = "Stop Recording";
-  actionBtn.removeEventListener("click", handleStart);
-  actionBtn.addEventListener("click", hanldeStop);
+  actionBtn.innerText = 'Stop Recording';
+  actionBtn.removeEventListener('click', handleStart);
+  actionBtn.addEventListener('click', hanldeStop);
 
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
@@ -105,9 +106,9 @@ const handleStart = () => {
 };
 
 const init = async () => {
-  actionBtn.innerText = "loading...";
-  actionBtn.removeEventListener("click", init);
-  actionBtn.addEventListener("click", handleStart);
+  actionBtn.innerText = 'loading...';
+  actionBtn.removeEventListener('click', init);
+  actionBtn.addEventListener('click', handleStart);
 
   actionBtn.disabled = true;
   stream = await navigator.mediaDevices.getUserMedia({
@@ -120,7 +121,7 @@ const init = async () => {
   video.srcObject = stream;
   video.play();
   actionBtn.disabled = false;
-  actionBtn.innerText = "Start Recording";
+  actionBtn.innerText = 'Start Recording';
 };
 
-actionBtn.addEventListener("click", init);
+actionBtn.addEventListener('click', init);
